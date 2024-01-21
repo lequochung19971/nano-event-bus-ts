8b7d316c572f144f87271eb0b83744a6e6c8658a
iamhung <43690592+lequochung19971@users.noreply.github.com>
2024-01-21 22:19:35 +0700

    fix: remove surplus code

--- index.ts
+++ index.ts
@@ -14,7 +14,6 @@ type ListenerInfo = {
 export const createNanoEventBus = () => {
   const emittedEvents = {} as Record<string, any>;
   const listeners = {} as Record<string, Record<string, ListenerInfo>>;
-  console.log('test release 2');
   /**
    *
    * @param event
